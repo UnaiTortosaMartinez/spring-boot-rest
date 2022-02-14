93a66a7ef399e00db61310eba2eb1911d79eec88
UnaiTortosaMartinez <95503815+UnaiTortosaMartinez@users.noreply.github.com>
2022-02-14 19:36:12 +0100

    a

---
+++ SpringHelloWorld/src/main/java/com/jcg/spring/example/SampleController.java
@@ -0,0 +1,19 @@
+package com.jcg.spring.example;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class SampleController {
+
+    @RequestMapping("/sample")
+    public Map<String,String> sample(@RequestParam(value="name", defaultValue="World") String name) {
+    Map<String,String> result = new HashMap<>();
+    result.put("message", String.format("Hello, %s", name));
+    return result;
+    }
+}
